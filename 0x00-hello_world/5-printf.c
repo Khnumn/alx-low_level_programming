0ece6f086dbbdaf63f566baabe2742e42e0b7fcb
Khnumn <konsciousbeatsoul@proton.me>
2022-10-13 18:13:09 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints a string using printf
+ * Return: 0 with no errors
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
