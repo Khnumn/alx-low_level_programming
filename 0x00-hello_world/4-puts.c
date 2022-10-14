0fb85261ebc2951fe23bae6c3f935b84609899d3
Khnumn <konsciousbeatsoul@proton.me>
2022-10-13 17:28:09 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main- Entry point
+ * Description:Uses puts to output a string text
+ * Return: returns 0 
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
