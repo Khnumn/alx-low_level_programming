9dfb1a84fdd7c12f6cc897fb64158ba6914e646a
Khnumn <konsciousbeatsoul@proton.me>
2022-10-13 17:51:23 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main- Entry point
- * Description:Uses puts to output a string text
- * Return: returns 0 
+ *  main - Entry point
+ *  Description:Uses puts to display string
+ *  Return: 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
+
