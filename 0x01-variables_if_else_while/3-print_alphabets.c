7c6280352854bced53ffc6e2f1ce655f438601fb
Khnumn <konsciousbeatsoul@proton.me>
2022-10-17 16:39:08 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>                                                                                                              
+#include <unistd.h>                                                                                                            
+/**
+ * main - Initialization
+ * Desc: prints upper and lowercase
+ * Return: 0 when success
+ */
+int main(void)
+{
+		int al;
+
+		for (al = 'a'; al <= 'z'; al++)
+		{
+			putchar(al);
+		}							
+		for (al = 'A'; al <= 'Z'; a++)
+		{
+			putchar(ch);
+		}
+		putchar('\n');
+		return (0);
+}
