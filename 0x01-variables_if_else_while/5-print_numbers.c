0086adbb0748664a81db2930262e40e84c3a8043
Khnumn <konsciousbeatsoul@proton.me>
2022-10-17 17:46:46 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Initialization
+ * Desc: print single numbers
+ * Return: 0 success
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
