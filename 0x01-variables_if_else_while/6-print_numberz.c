ebd34f910ac187c9b069e7b43c3031bbade72e58
Khnumn <konsciousbeatsoul@proton.me>
2022-10-17 17:59:41 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main -Initialz
+ * Desc: print single digits base 10
+ * Return: 0 successful
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10, c++)
+	{
+		putchar(c + '0');
+	}
+	putchar('\n');
+	return (0);
+}
