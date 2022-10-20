e4437b6a083f8f3e2f89ee74593fefe5789de306
Khnumn <konsciousbeatsoul@proton.me>
2022-10-20 16:20:06 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+ * main - Initialization
+ * Desc: displays positive or negative integers
+ * Return: 0 success no errors
+ */
+int main(void)
+{
+	int x;
+
+	x =0;
+	positive_or_negative(x);
+
+	return (0);
+}
