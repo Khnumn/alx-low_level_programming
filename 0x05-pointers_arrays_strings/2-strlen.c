c5829cee00afd114c5a97d363fd467b627b699b5
Khnumn <konsciousbeatsoul@proton.me>
2022-10-24 15:36:02 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _strlen-  function that returns the length of a string
+ *@s: string variable
+ * Return : 0 succcess
+ */
+
+int _strlen(char *s)
+{
+	int lenz = 0;
+
+	for (lenz = 0; str[lenz]; lenz++)
+	{
+		return (lenz);
+	}
+}
