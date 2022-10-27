d746450b99dc684298c82f63aacf9775619c0517
Khnumn <konsciousbeatsoul@proton.me>
2022-10-27 16:37:58 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,8 +7,11 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int a = 0, dest_len =0;
+	int a = 0;
+	dest_len =0;
 
+	while (dest[a++])
+		dest_len++;
        	for (a = 0 ; src[a] != '\0' ; a++)
 		dest[dest_len + a] = src[a];
 	 dest[dest_len + a] = '\0';
