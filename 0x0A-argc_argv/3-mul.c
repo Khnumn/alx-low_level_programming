209c3c19b237e17b20f54db9e4d4c7cdaed52bae
Khnumn <konsciousbeatsoul@proton.me>
2022-11-10 03:48:45 -0800

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * main - program that multiplies two numbers.
  *@argc: Number of command line arguments
  *@argv: Array of argc
  *Return: 0 success
  */
+
 int main(int argc, char *argv[])
 {
 	int x, y;
